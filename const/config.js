f8dfc09003199e681d453427c0743602502cda24
KeletsoMakofane <65560061+KeletsoMakofane@users.noreply.github.com>
2023-05-16 18:47:55 -0400

    added matrix question

--- const/config.js
+++ const/config.js
@@ -4,8 +4,8 @@ import {mapStyles} from "./mapStyles";
 export const config = {
     testing: true,
     saveThrottleDuration: 1000*10,
-    colorBackground: "#FF99C5",
-    colorText: "#5D207A",
+    colorBackground: "#5D207A",
+    colorText: "#FF99C5",
     cookieDuration : {
         testing: 60*10,
         production: 60*60*24*29*6
